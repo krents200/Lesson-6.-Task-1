b4f57858d8632fb12b14202432e56e008eafb57b
OZON Pedagog <OZON Pedagog@DESKTOP-TENT9N6>
2024-01-08 17:39:21 +0500

    Добавьте файлы проекта.

---
+++ Lesson_6._Task_1.py
@@ -0,0 +1,9 @@
+﻿print ('Введите количество чисел:')
+N = int (input () )
+quanity = 0
+for i in range (N):
+    print ('Введите', i + 1, 'число:')
+    n = int (input () )
+    if n == 0:
+        quanity += 1
+print ('Количество нулей:', quanity, sep = '\n')
